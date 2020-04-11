523ddf098e53b8cf8638a9f45a1cde71222ef3b6
Murphy <murphysunx@gmail.com>
2020-04-11 12:12:30 +1000

    resource present page

--- Virtual Library/app/src/main/java/com/example/virtuallibrary/MainActivity.java
+++ Virtual Library/app/src/main/java/com/example/virtuallibrary/MainActivity.java
@@ -2,7 +2,18 @@ package com.example.virtuallibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.resource.lib.ResourceManager;
+import com.resource.lib.ResourceManagerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.jumpToResourceList();
     }
+
+    /**
+     * jump
+     */
+    private void jumpToResourceList() {
+        Intent intent = new Intent(MainActivity.this, ResourceListActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
