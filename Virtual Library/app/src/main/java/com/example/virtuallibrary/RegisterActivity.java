fe2a17674ed85b3ba4686000ef6278c67cb171bb
Zhijia Lu <624636394@qq.com>
2020-04-11 10:57:15 +1000

    login finish

--- Virtual Library/app/src/main/java/com/example/virtuallibrary/RegisterActivity.java
+++ Virtual Library/app/src/main/java/com/example/virtuallibrary/RegisterActivity.java
@@ -1,16 +1,4 @@
 package com.example.virtuallibrary;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,8 +7,12 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
@@ -32,26 +24,34 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
-    private Button loginBtn;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class RegisterActivity  extends AppCompatActivity {
+    private Button registerBin;
     private TextInputEditText usernameInput;
-    private TextInputLayout passwordLayout;
     private TextInputEditText passwordInput;
+    private TextInputEditText nameInput;
+    private TextInputEditText accountInput;
     private OkHttpClient client;
     private LoginViewModel loginViewModel;
-    private TextView registerLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.register);
 
-        loginBtn = findViewById(R.id.login_btn);
-        loginBtn.setEnabled(false);
+        registerBin = findViewById(R.id.register_btn);
+        registerBin.setEnabled(false);
         usernameInput = findViewById(R.id.usernameInput);
-        passwordLayout = findViewById(R.id.passwordLayout);
         passwordInput = findViewById(R.id.passwordInput);
-        registerLink = findViewById(R.id.register);
+        nameInput = findViewById(R.id.nameInput);
+        accountInput = findViewById(R.id.accountTypeInput);
         client = new OkHttpClient();
 
         /*
@@ -60,31 +60,24 @@ public class MainActivity extends AppCompatActivity {
 
          */
 
-        registerLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(intent);
-                finish();
-                    }
-
-
-        });
-
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        registerBin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String username = usernameInput.getText().toString();
                 String password = passwordInput.getText().toString();
-                Log.d("login", "onClick: " + password);
+                String name = nameInput.getText().toString();
+                String accountType = accountInput.getText().toString();
+                Log.d("register", "onClick: " + password);
                 RequestBody requestBody = new FormBody.Builder()
                         .add("username", username)
                         .add("password", password)
+                        .add("name", name)
+                        .add("accountType", accountType)
                         .build();
 
-                String loginUrl = "https://techtailors.sytes.net:8400/users/login";
+                String registerUrl = "https://techtailors.sytes.net:8400/users/register";
                 Request request = new Request.Builder()
-                        .url(loginUrl)
+                        .url(registerUrl)
                         .post(requestBody)
                         .build();
 
@@ -95,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void run() {
 
-                                Toast.makeText(MainActivity.this, "Log in failed, try it later", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(RegisterActivity.this, "Log in failed, try it later", Toast.LENGTH_SHORT).show();
                             }
                         });
                     }
@@ -103,31 +96,32 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                         String responseData =response.body().string();
-                        Log.d("response", "login response: "+ responseData);
+                        Log.d("response", "register response: "+ responseData);
                         try {
                             final JSONObject jsonData = new JSONObject(responseData);
+                            String userID = jsonData.getString("_id");
+                            String token = jsonData.getString("password");
 
-
-                            Intent intent = new Intent(MainActivity.this, RoomList.class);
+                            Intent intent = new Intent(RegisterActivity.this, RoomList.class);
                             intent.putExtra("userName", jsonData.getString("username"));
                             startActivity(intent);
                             finish();
 //
 //                            if (result){
-//
+
 //                            }
 //                            else{
-//                                Toast.makeText(MainActivity.this, "Failed, please verify your code", Toast.LENGTH_SHORT).show();
+//                                Toast.makeText(RegisterActivity.this, "Failed, please verify your code", Toast.LENGTH_SHORT).show();
 //                                runOnUiThread(new Runnable() {
 //                                    @Override
 //                                    public void run() {
 //
-//                                        Toast.makeText(MainActivity.this, "Failed, please verify your code", Toast.LENGTH_SHORT).show();
+//                                        Toast.makeText(RegisterActivity.this, "Failed, please verify your code", Toast.LENGTH_SHORT).show();
 //                                    }
 //                                });
 //
 //                            }
-//
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -148,9 +142,9 @@ public class MainActivity extends AppCompatActivity {
                 if (loginFormState == null) {
                     return;
                 }
-                loginBtn.setEnabled(loginFormState.isDataValid());
+                registerBin.setEnabled(loginFormState.isDataValid());
                 if (loginFormState.getUsernameError() != null) {
-                   usernameInput.setError(getString(loginFormState.getUsernameError()));
+                    usernameInput.setError(getString(loginFormState.getUsernameError()));
                 }
                 if (loginFormState.getPasswordError() != null) {
                     passwordInput.setError(getString(loginFormState.getPasswordError()));
@@ -177,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
         };
 
         usernameInput.addTextChangedListener(afterTextChangedListener);
-       passwordInput.addTextChangedListener(afterTextChangedListener);
+        passwordInput.addTextChangedListener(afterTextChangedListener);
 
 
 
